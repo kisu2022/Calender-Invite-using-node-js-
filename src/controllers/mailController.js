a52bc980dc3bd89e3ca36d0d07ea691f148608f4
kisu@2022 <kisunano@gmail.com>
2023-12-18 18:11:07 +0530

    delete unnecessary content in file

--- src/controllers/mailController.js
+++ src/controllers/mailController.js
@@ -4,53 +4,13 @@ const ICAL = require('ical.js');
 const ics = require('ics');
 
 const senderConfig= require("../../config.json")
-//const { createEvent, createIcal } = require('../config/ical-helper');
 
-// let content = 'BEGIN:VCALENDAR\r\nPRODID:-//ACME/DesktopCalendar//EN\r\nMETHOD:REQUEST\r\n...';
+
+
 
 const sendmail = async (req, res) => {
     const { userEmail,subject,hour,minutes} = req.body;
 
-    // const event = {
-    //     _id: '123',
-    //     DTStart: '20231218T160000Z',
-    //     DTEnd: '220231218T170000Z',
-    //     DTStamp: '20230101T000000Z',
-    //     organizerName: 'Entiovi test',
-    //     organizerEmail: 'kchatterjee@entiovi.com',
-    //     attendeeName: 'Krishanu Chatterjee',
-    //     attendeeEmail: 'kchatterjee@entiovi.com',
-    //     body: 'Event details',
-    //     location: 'Kolkata',
-    //     sequence: '1',
-    //     title: 'Sheduled Interview',
-    //   };
-    // //ical format for google and outlook
-    // const iCal = 
-    // `
-    // BEGIN:VCALENDAR
-    // PRODID:-//Cratic//_Scheduler//EN
-    // VERSION:2.0
-    // CALSCALE:GREGORIAN
-    // METHOD:REQUEST
-    // BEGIN:VEVENT
-    // DTSTART:${event.DTStart}
-    // DTEND:${event.DTEnd}
-    // DTSTAMP:${event.DTStamp}
-    // ORGANIZER;CN=${event.organizerName}:mailto:${event.organizerEmail}
-    // UID:${event._id}@url.com
-    // ATTENDEE;CUTYPE=INDIVIDUAL;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=${event.attendeeName};X-NUM-GUESTS=0:mailto:${event.attendeeEmail}
-    // CREATED:${event.DTStamp}
-    // DESCRIPTION:${event.body}
-    // LAST-MODIFIED:${event.DTStamp}
-    // LOCATION:${event.location}
-    // SEQUENCE:${event.sequence}
-    // STATUS:CONFIRMED
-    // SUMMARY:${event.title}
-    // TRANSP:OPAQUE
-    // END:VEVENT
-    // END:VCALENDAR
-    // `;
 
     let config = {
         service : 'gmail',
@@ -77,40 +37,7 @@ const sendmail = async (req, res) => {
     console.log(value)
     let transporter = nodemailer.createTransport(config);
 
-    // //creating ical file using ical.js  library ....ading a function which will create the file ....
-    // function createICalEvent() {
-    //     const jcalData = {
-    //       jcal: {
-    //         method: 'REQUEST',
-    //         prodid: '//My//Product//EN',
-    //         version: '2.0',
-    //         events: [{
-    //           start: '20231218T150000Z',
-    //           end: '20231218T160000Z',
-    //           summary: 'Sample Event',
-    //           location: 'Sample Location',
-    //           description: 'This is a sample calendar invitation.',
-    //           organizer: 'mailto:organizer@example.com',
-    //           attendees: [
-    //             {
-    //               email: 'kchatterjee@entiovi.com',
-    //             },
-    //             {
-    //               email: 'kisusanuchatthot@hotmail.com',
-    //             },
-    //           ],
-    //         }],
-    //       },
-    //     };
-      
-    //     const jcalDataString = ICAL.stringify(jcalData);
-    //     const jcalComponent = ICAL.Component.fromString(jcalDataString);
-    //     return jcalComponent;
-    //   }
-    //.....................................................................................
-    // const buf = Buffer.from(iCal);
-    // const base64Cal = buf.toString('base64');
-
+    
 
   
 
@@ -123,13 +50,7 @@ const sendmail = async (req, res) => {
             filename: 'invitation.ics',
             method: 'request',
             content: value,
-            //encoding:'base64'
         },
-        // attachments: [{
-        //     name: 'invite.ics',
-        //     type: 'text/calendar;method=REQUEST;name="invite.ics"',
-        //     data: base64Cal,
-        //   }],
       }
 
    
